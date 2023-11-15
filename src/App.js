92ec7f90da0e93c55268aeca30b2458f49f05982
Scott Abbott <scott@essaymediawv.com>
2023-11-14 22:46:52 -0500

    First draft

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
+import logo from './EssayMedia-1280.png';
+import instagram from './Instagram_White.png';
+import facebook from './Facebook_White.png';
+import galleries from './Galleries.png';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="Essay Media" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Photography & Videography
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="mailto:scott@essaymediawv.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Contact
         </a>
+        <p>
+          <div class="Social-link">
+          <a
+            className="Social-link"
+            href="https://www.scottabbott.photo/browse/"
+            rel="noopener noreferrer"
+          >
+            <img src= {galleries} className="Socials" alt="Browse Photo Galleries at Scott Abbott Photo" />
+          </a> 
+          </div>
+          <div class="Social-link">
+          <a className="Social-link"
+          href="https://www.instagram.com/essaymediawv/"
+          rel="noopener noreferrer"
+          >
+            <img src={instagram} className="Socials" alt="Instagram" />
+          </a>
+          </div>
+          <div class="Social-link">
+          <a className="Social-link"
+          href="https://www.facebook.com/essaymediawv/"
+          rel="noopener noreferrer"
+          >
+            <img src={facebook} className="Socials" alt="Facebook" />
+          </a>
+          </div>
+        </p>
       </header>
     </div>
   );
